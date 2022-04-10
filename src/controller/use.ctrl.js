9e618089e0017d7bdfd6e151c4a53f500643278d
arpitChokniwal <arpitchokniwal09@gmail.com>
2022-04-10 17:33:46 +0530

    cmd

--- src/controller/use.ctrl.js
+++ src/controller/use.ctrl.js
@@ -42,10 +42,24 @@ rout.get('/',async(req,res)=>{
     try{
         const AllUser = await User.find() .populate({ path:  "jobSchemaId" , select: [ "companyName" ,  "jobTitle" , "city" , "salary" ],populate:{path: "adminSchemaId" ,select:[ "companyName" , "userName" , "email" ]}  }).lean().exec();
 
-        return res.status(201).send({AllUser})
+        return res.status(201).send(AllUser)
 
     }catch(e){
      res.status(400).send(e.message)       
+    }
+})
+
+
+
+rout.get('/:id',async(req,res)=>{
+    try{
+        const AllJob = await User.find({jobSchemaId:req.params.id}).populate({ path: "jobSchemaId", select: ["companyName", "city", "jobTitle","salary"],populate:{path: "adminSchemaId", select: ["companyName", "userName", "email"]} }).lean().exec()
+        // console.log(AllJob)
+        
+        return res.status(201).send(AllJob)
+
+    }catch(e){
+        res.status(400).send(e.message)
     }
 })
 
