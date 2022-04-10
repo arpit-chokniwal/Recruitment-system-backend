d23dc7e696b403b8fa5559e8e7179e0e5637492d
arpitChokniwal <arpitchokniwal09@gmail.com>
2022-04-10 16:59:50 +0530

    cba

--- src/models/job.model.js
+++ src/models/job.model.js
@@ -6,8 +6,8 @@ const jobSchema = new mongoose.Schema({
     "city":{ type:String, required:true },
     "jobTitle":{ type:String, required:true },
     "jobDescription":{ type:String, required:true },
-    "experience":{ type:Number, required:true },
-    "salary":{ type:Number, required:true },
+    "experience":{ type:String, required:true },
+    "salary":{ type:String, required:true },
     "companyImageUrl":{ type:String, required:true },
     "dateOfPost":{ type:String, required:true },
     "adminSchemaId":{
